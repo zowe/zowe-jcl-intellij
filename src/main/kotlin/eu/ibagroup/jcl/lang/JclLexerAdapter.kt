bf3ce24c2a5c2e388eb9cfa7fd8c17e3313982eb
Krus_V <valiantsin.krus@ibm.com>
2022-05-31 17:18:41 +0300

    small improvement

--- src/main/kotlin/eu/ibagroup/jcl/lang/JclLexerAdapter.kt
+++ src/main/kotlin/eu/ibagroup/jcl/lang/JclLexerAdapter.kt
@@ -4,8 +4,13 @@ import com.intellij.lexer.FlexAdapter
 
 class JclLexerAdapter(): FlexAdapter(JclLexer(null)) {
   override fun start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int) {
-    (flex as JclLexer).yycolumn = 0
     (flex as JclLexer).yyline = 0
+    (flex as JclLexer).yycolumn = 0
+    (flex as JclLexer).prevState = 0
+    (flex as JclLexer).tupleInnerCounter = 0
+    (flex as JclLexer).movedBack = false
+    (flex as JclLexer).instreamParamStarted = false
+    (flex as JclLexer).firstParamInitialized = false
     super.start(buffer, startOffset, endOffset, initialState)
   }
 }
