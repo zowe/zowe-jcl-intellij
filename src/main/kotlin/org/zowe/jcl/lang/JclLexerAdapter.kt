caefabf7f58b0395a9a87cce0ed3a00d5ae09050
dnlsv <70668087+dnlsv@users.noreply.github.com>
2023-06-02 14:39:36 +0300

    IJMP-1022: add task to generate lexer with encoding set (#3)
    * IJMP-1022: add task to generate lexer with encoding set
    
    Signed-off-by: Denis Lisenkov <lisenkov.d.y@gmail.com>
    
    * IJMP-918: improved if condition
    
    Signed-off-by: Denis Lisenkov <lisenkov.d.y@gmail.com>
    
    * IJMP-1022: small fix
    
    Signed-off-by: Denis Lisenkov <lisenkov.d.y@gmail.com>
    
    ---------
    
    Signed-off-by: Denis Lisenkov <lisenkov.d.y@gmail.com>

--- src/main/kotlin/org/zowe/jcl/lang/JclLexerAdapter.kt
+++ src/main/kotlin/org/zowe/jcl/lang/JclLexerAdapter.kt
@@ -14,8 +14,6 @@ import com.intellij.lexer.FlexAdapter
 
 class JclLexerAdapter(): FlexAdapter(JclLexer(null)) {
   override fun start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int) {
-    (flex as JclLexer).yyline = 0
-    (flex as JclLexer).yycolumn = 0
     (flex as JclLexer).prevState = 0
     (flex as JclLexer).tupleInnerCounter = 0
     (flex as JclLexer).movedBack = false
